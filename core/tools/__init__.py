3b0d7e9482dfadb36e40acc64dad5df89996f352
huguotao7412 <2961606178@qq.com>
2026-07-01 13:32:37 +0800

    feat(tools): register UpdateStateTool in tools __init__

---
+++ core/tools/__init__.py
@@ -0,0 +1,19 @@
+from .read import ReadTool
+from .write import WriteTool
+from .edit import EditTool
+from .bash import BashTool
+from .search import SearchCodebaseTool
+from .list_dir import ListDirTool
+from .read_outline import ReadOutlineTool
+from .update_state import UpdateStateTool
+
+__all__ = [
+    "ReadTool",
+    "WriteTool",
+    "EditTool",
+    "BashTool",
+    "SearchCodebaseTool",
+    "ListDirTool",
+    "ReadOutlineTool",
+    "UpdateStateTool",
+]
